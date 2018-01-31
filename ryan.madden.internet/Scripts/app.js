bd58dd79348bf296211448e2500422550c6f5ed5
ryan.madden <ryan.madden@5Q3BCG2>
2018-01-31 09:36:31 -0500

    404 guerdd upfdated

--- ryan.madden.internet/Scripts/app.js
+++ ryan.madden.internet/Scripts/app.js
@@ -1,5 +1,4 @@
-﻿
-//........................resume....................................
+﻿//........................resume....................................
 $("#infoExpand").hide();
 $("#eduExpand").hide();
 $("#expExpand").hide();
@@ -35,37 +34,46 @@ $("#expandBtn").click(function () {
 
 var answer = "IEHOVA";
 var altAnswer = "RYAN";
-var guess = ""; //  <h3 id="name">The Name of God is: (user answer built here)</h3>
+var guess = ""; //  ID updateName
+
 
 $("#pathPic").hide();
 $("#footSteps").hide();
 $("#name").hide();
 $("#404").hide();
-
+$("#updateName").hide();
 $("#fail").hide();
 $("#pass").hide();
 
 $(document).ready(function ()
-{
+{   
     $("#pathPic").slideDown(5000);
     $("#footSteps").fadeIn(10000);
     $("#name").fadeIn(10000);
-    $("#404").fadeIn(10000);    
+    $("#404").fadeIn(10000);  
+    $("#updateName").fadeIn(10000);
+
+    $("#404 > .yes").click(function () {
+            var letter = "";
+            letter = this.innerText;
+            letter = letter.substring(0, 1);
+            guess = guess + letter;
+
+            $("#updateName").text(guess);
+
+
+            console.log("guess = " + guess);
+            if (guess == altAnswer || guess == answer) {
+                $("#puzzle").fadeOut(1000);
+                $("#pass").fadeIn(5000);
+                guess = "";
+            }
+    });
+    $("#404 > .no").click(function () {
+        $("#puzzle").fadeOut(1000);
+        $("#fail").fadeIn(5000);
+        guess = "";
+    })
+   
+
 });
-
-
-$("#404 > .yes").click(function () {
-        var letter = "";
-        letter = this.innerText;
-        letter = letter.substring(0, 1);
-        guess = guess + letter;
-        
-
-        console.log("guess = " + guess);
-        if (guess == altAnswer || guess == answer) {
-            $("#puzzle").fadeOut(1000);
-            $("#pass").fadeIn(5000);
-            guess = "";
-        }
-    });   
-
