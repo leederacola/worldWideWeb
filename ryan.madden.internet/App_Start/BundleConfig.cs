213455339a508c327889ea38a5e71b83709289d5
ryan.madden <ryan.madden@5Q3BCG2>
2018-02-12 14:16:47 -0500

    changes made

--- ryan.madden.internet/App_Start/BundleConfig.cs
+++ ryan.madden.internet/App_Start/BundleConfig.cs
@@ -12,11 +12,7 @@ namespace ryan.madden.internet
             bundles.UseCdn = true;
             var jQueryCdnPath = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
             var bootStrapCdnPath = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
-            
-            /** Default jQuery 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
-            **/
-
+         
             //  overloaded jQ bundle with cdn path
             bundles.Add(new ScriptBundle("~/bundles/jQuery", jQueryCdnPath).Include(
                 "~/Scripts/jQuery-{version}.js"));
@@ -29,18 +25,18 @@ namespace ryan.madden.internet
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            /**     Deafualt Bootstrap
-             * 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include( "~/Scripts/bootstrap.js","~/Scripts/respond.js"));
-            */
-
             // overloaded BS4 with cdn path
             bundles.Add(new ScriptBundle("~/bundles/bootstrap", bootStrapCdnPath).Include(
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custonJS").Include("~/Scrips/app.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
 
             // this guy needs to be true to min and bundle
             // it overrides the webConfig <compilation debug="true"/>
--- ryan.madden.internet/Scripts/app.js
+++ ryan.madden.internet/Scripts/app.js
@@ -1,30 +1,9 @@
 ﻿//........................resume....................................
-$("#infoExpand").hide();
-$("#eduExpand").hide();
-$("#expExpand").hide();
-$("#otherExpand").hide();
 
+$(".resumeToggle").hide();
 
-// got to be more efficent way for this............other than function for every element
-$("#info").click(function () {
-    $("#infoExpand").slideToggle(1000);
-})
-$("#edu").click(function () {
-    $("#eduExpand").slideToggle(1000);
-})
-$("#exp").click(function () {
-    $("#expExpand").slideToggle(1000);
-})
-$("#other").click(function () {
-    $("#otherExpand").slideToggle(1000);
-})
-
-
-$("#expandBtn").click(function () {
-    $("#infoExpand").slideToggle(1000);
-    $("#eduExpand").slideToggle(1000);
-    $("#expExpand").slideToggle(1000);
-    $("#otherExpand").slideToggle(1000);
+$(".resumeToggle").cick(function () {  
+    $(".resumeToggle").slideToggle(1000);
 })
 
 
